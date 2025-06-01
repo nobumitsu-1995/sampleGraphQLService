11e6184490a708f80497a61df80fa3f69cbcbc81
nobumitsu_masugata <ys.training220208@gmail.com>
2025-06-01 09:12:23 +0900

    feat: サーバーの雛形を作成

---
+++ server/src/index.ts
@@ -0,0 +1,32 @@
+import { ApolloServer } from 'apollo-server-express'
+import express from 'express'
+import { buildSchema } from 'type-graphql'
+
+const main = async () => {
+  const app = express()
+
+  // resolvers をインポートまたは定義する必要があります。例として空配列を指定します。
+  const schema = await buildSchema({
+    resolvers: [
+      () => {}
+    ],
+    emitSchemaFile: true,
+  })
+
+  const server = new ApolloServer({
+    schema
+  })
+
+  await server.start()
+  server.applyMiddleware({ app })
+
+  const PORT = 4000
+
+  app.listen(PORT, () => {
+    console.log(`🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`)
+  })
+}
+
+main().catch(error => {
+  console.error(error)
+})
